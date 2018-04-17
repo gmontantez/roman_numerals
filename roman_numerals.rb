a410adb70d76961f4a6cace82842600fdf86c65e
gmontantez <gmontantez7@gmail.com>
2018-04-17 09:27:05 -0400

    Roman Numerals Exercise Beginning

---
+++ roman_numerals.rb
@@ -0,0 +1,3 @@
+def roman_numberals
+	
+end
---
+++ roman_numerals_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "roman_numerals.rb"
+
+class Roman_numberals_test < Minitest::Test
+	def test_true_equals_true
+		assert_equal(true,true)
+	end
+end
+	# def test_
+	# 	assert_equal(true,true)
+	# end
+
+
